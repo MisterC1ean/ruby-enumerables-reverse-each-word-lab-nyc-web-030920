bcd891220e91a5de06ce61d733aa74bea548f39a
Robert Keller <robert_keller44@icloud.com>
2020-01-17 20:50:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,9 @@
 def reverse_each_word(sentence)
   new_array = []
   string_array = sentence.split
-  string_array.each do |x|
+  string_array.collect do |x|
     new_array << x.reverse
   end
   new_array.join(" ")
 end
+
